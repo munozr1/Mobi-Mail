758e3cd25c89ee72d1b51abe7b2a0e14f7c5b12b
Rodrigo Munoz <rm117579@gmail.com>
2024-02-19 03:15:31 +0000

    init

---
+++ client.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main() {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        exit(EXIT_FAILURE);
+    }
+
+    char *message = "Hello from client";
+    send(sock, message, strlen(message), 0);
+    printf("Hello message sent\n");
+
+    char buffer[1024] = {0};
+    ssize_t valread = recv(sock, buffer, 1024, 0);
+    printf("%s\n", buffer);
+
+    close(sock);
+    return 0;
+}
+
