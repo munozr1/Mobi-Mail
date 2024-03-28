4c95e67c33348f891cf7b21a4dd856df30f52652
Rodrigo Munoz <rm117579@gmail.com>
2024-03-28 16:02:31 -0500

    fix: add npm start script to backend

--- backend/pop3.js
+++ backend/pop3.js
@@ -1,21 +1,21 @@
 const sqlite3 = require('sqlite3')
 
+let db = new sqlite3.Database('emaildb');
+
+
 function processCommand(command) {
   let db = new sqlite3.Database('emaildb');
   switch(command) {
     case "USER":
-      break;
-    case "PASS":
-      //TODO:commands.PASS();
-      break;
-    case "STAT":
-     //TODO: commands.STAT();
       break;
     case "QUIT":
      //TODO: commands.QUIT();
       break;
     case "LIST":
      //TODO: commands.LIST();
+      break;
+    case "RETR":
+      //TODO: commands.RETR();
       break;
     default:
       // Handle unknown command
